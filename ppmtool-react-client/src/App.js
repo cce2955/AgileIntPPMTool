d27ebe23a67064a4826f6175deb664fb596974bf
Everett <christopher.everett@henry.k12.ga.us>
2020-05-13 23:16:15 -0400

    Basic react routing, css, and components, learning the system

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Dashboard from './components/Dashboard';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 class App extends Component {
-  render(){
+  render() {
     return (
-      <div className="App">
-        <Dashboard/>
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
     );
   }
 }
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import CreateProjectButton from "./Project/CreateProjectButton";
 
 export default class Dashboard extends Component {
-    render() {
-        return (
-            <h1> Welcome to the Dashboard</h1>
-
-        )
-        
-    }   
+  render() {
+    return (
+      <div>
+        <div className="projects">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="display-4 text-center">Projects</h1>
+                <br />
+                <CreateProjectButton />
+                <br />
+                <hr />
+                <ProjectItem />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
