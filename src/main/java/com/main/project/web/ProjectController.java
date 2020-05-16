b6f3a47f713f6a18c19f37d15ac4da9fa0368868
Everett <christopher.everett@henry.k12.ga.us>
2020-05-15 23:52:45 -0400

    React Redux base setup

--- src/main/java/com/main/project/web/ProjectController.java
+++ src/main/java/com/main/project/web/ProjectController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,7 +29,7 @@ import com.main.project.services.ProjectService;
 
 
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
